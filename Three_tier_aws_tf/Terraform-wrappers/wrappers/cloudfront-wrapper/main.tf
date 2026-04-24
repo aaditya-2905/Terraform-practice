e52be0d432ec1c4e39e1b3c11b33830f5a8b55dc
Aadityasinh zala <zalaaadityasinh9@gmail.com>
2026-04-24 15:30:23 +0530

    Wrappers for project

---
+++ Three_tier_aws_tf/Terraform-wrappers/wrappers/cloudfront-wrapper/main.tf
@@ -0,0 +1,49 @@
+locals {
+  # Map-based configuration for CloudFront distributions
+  distributions = var.distributions
+
+  # Common tags for all resources
+  common_tags = merge(
+    var.common_tags,
+    {
+      created_by = "terraform"
+      module     = "cloudfront-wrapper"
+    }
+  )
+}
+
+module "distribution" {
+  for_each = local.distributions
+
+  source = "aaditya-2905/cloudfront/aws"
+
+  aliases                 = each.value.aliases
+  origin                  = each.value.origin
+  origin_group            = each.value.origin_group
+  origin_access_control   = each.value.origin_access_control
+  default_cache_behavior  = each.value.default_cache_behavior
+  ordered_cache_behavior  = each.value.ordered_cache_behavior
+  custom_error_response   = each.value.custom_error_response
+  restrictions            = each.value.restrictions
+  viewer_certificate      = each.value.viewer_certificate
+  response_headers_policy = each.value.response_headers_policy
+  custom_headers          = each.value.custom_headers
+  logging_config          = each.value.logging_config
+  log_delivery            = each.value.log_delivery
+  vpc_origin              = each.value.vpc_origin
+
+  tags = merge(
+    local.common_tags,
+    each.value.tags
+  )
+}
+
+output "distribution_ids" {
+  description = "IDs of created CloudFront distributions"
+  value       = { for k, v in module.distribution : k => v.distribution_id }
+}
+
+output "distribution_domain_names" {
+  description = "Domain names of created CloudFront distributions"
+  value       = { for k, v in module.distribution : k => v.domain_name }
+}
---
+++ Three_tier_aws_tf/Terraform-wrappers/wrappers/sg-wrapper/main.tf
@@ -0,0 +1,47 @@
+locals {
+  # Merge defaults with user inputs
+  sgs = {
+    for key, config in var.sgs : key => merge(
+      {
+        description   = key
+        ingress_rules = []
+        egress_rules = [
+          {
+            from_port   = 0
+            to_port     = 0
+            protocol    = "-1"
+            cidr_blocks = ["0.0.0.0/0"]
+          }
+        ]
+        tags = {}
+      },
+      config
+    )
+  }
+
+  # Common tags for all resources
+  common_tags = merge(
+    var.common_tags,
+    {
+      created_by = "terraform"
+      module     = "sg-wrapper"
+    }
+  )
+}
+
+module "sg" {
+  for_each = local.sgs
+
+  source = "aaditya-2905/sg/aws"
+
+  vpc_id        = each.value.vpc_id
+  ingress_rules = each.value.ingress_rules
+  egress_rules  = each.value.egress_rules
+  environment   = each.value.environment
+
+}
+
+output "sg_ids" {
+  description = "IDs of created Security Groups"
+  value       = { for k, v in module.sg : k => v.sg_id }
+}
